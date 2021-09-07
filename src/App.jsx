da0686045855641655a445990c0cdaee8c5914e8
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-08 03:31:34 +0700

    latihan-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,54 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import gsap, { Power2 } from 'gsap';
 
 const App = () => {
-  const image1 = useRef(null);
-  const image2 = useRef(null);
-  const image3 = useRef(null);
-  const image4 = useRef(null);
+  const container = useRef(null);
+  const text1 = useRef(null);
+  const text2 = useRef(null);
+  const text3 = useRef(null);
 
   useEffect(() => {
-    gsap.from([image1.current, image2.current, image3.current, image4.current], {
-      transform: 'translateX(120%)',
-      duration: 1.3,
-      skewX: 5,
-      opacity: 0,
-      ease: Power2.easeInOut,
+    gsap.from([text1.current, text2.current, text3.current], {
+      delay: 0.1,
+      duration: 0.85,
       stagger: 0.1,
+      ease: Power2.easeInOut,
+      y: 100,
+      opacity: 0,
     });
   }, []);
 
   return (
-    <div className="w-full h-[1200px] bg-red-500 flex justify-center items-center">
-      <div className="w-[400px] h-[500px] relative">
-        <img
-          ref={image1}
-          src="https://images.pexels.com/photos/3768163/pexels-photo-3768163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="cool"
-          className="w-[300px] h-[400px] object-cover object-center absolute top-[200px] left-[200px] z-50"
-        />
-        <img
-          ref={image2}
-          src="https://images.pexels.com/photos/4672246/pexels-photo-4672246.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="cool"
-          className="w-[260px] h-[350px] object-cover object-center absolute top-[-200px] left-[-225px] z-50"
-        />
-        <img
-          ref={image3}
-          src="https://images.pexels.com/photos/2922301/pexels-photo-2922301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="cool"
-          className="w-[300px] h-[400px] object-cover object-center absolute top-[-260px] left-[250px] z-50"
-        />
-        <img
-          ref={image4}
-          src="https://images.pexels.com/photos/4754648/pexels-photo-4754648.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="cool"
-          className="w-[300px] h-[400px] object-cover object-center absolute top-[250px] left-[-290px] z-50"
-        />
+    <div className="w-full h-screen bg-red-500 flex justify-center items-center">
+      <div
+        ref={container}
+        className="font-manrope font-semibold text-5xl flex flex-col text-center"
+      >
+        <span
+          className="overflow-hidden relative py-2 inline-block"
+        >
+          <span
+            ref={text1}
+            className="relative"
+          >
+            Otivar is <i className="font-spectral font-medium">contemporary</i> digital
+          </span>
+        </span>
+        <span
+          className="py-2 overflow-hidden"
+
+        >
+          <span ref={text2}>
+            landscape we devised for all
+          </span>
+        </span>
+        <span
+          className="font-spectral font-medium py-2 overflow-hidden"
+        >
+          <span ref={text3}>
+            <i>your projects</i>
+          </span>
+        </span>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        spectral: ['Spectral', 'serif'],
+      },
+    },
   },
   variants: {
     extend: {},
