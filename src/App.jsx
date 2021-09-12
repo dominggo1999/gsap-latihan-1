08a6544c5bd053282b21b8454afca0b435e6b5a9
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-12 22:02:39 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,27 @@
 import React, { useRef, useEffect, useState } from 'react';
-import gsap, { Power2 } from 'gsap';
+import gsap, { Power3 } from 'gsap';
 
 const App = () => {
-  const container = useRef(null);
-  const text1 = useRef(null);
-  const text2 = useRef(null);
-  const text3 = useRef(null);
+  const ref = useRef(null);
 
   useEffect(() => {
-    gsap.from([text1.current, text2.current, text3.current], {
-      delay: 0.1,
-      duration: 0.85,
-      stagger: 0.1,
-      ease: Power2.easeInOut,
-      y: 100,
-      opacity: 0,
+    gsap.from(ref.current, {
+      duration: 1,
+      ease: Power3.easeInOut,
+      x: '200px',
+      width: 0,
     });
   }, []);
 
   return (
     <div className="w-full h-screen bg-red-500 flex justify-center items-center">
-      <div
-        ref={container}
-        className="font-manrope font-semibold text-5xl flex flex-col text-center"
-      >
-        <span
-          className="overflow-hidden relative py-2 inline-block"
-        >
-          <span
-            ref={text1}
-            className="relative"
-          >
-            Otivar is <i className="font-spectral font-medium">contemporary</i> digital
-          </span>
-        </span>
-        <span
-          className="py-2 overflow-hidden"
-
-        >
-          <span ref={text2}>
-            landscape we devised for all
-          </span>
-        </span>
-        <span
-          className="font-spectral font-medium py-2 overflow-hidden"
-        >
-          <span ref={text3}>
-            <i>your projects</i>
-          </span>
-        </span>
+      <div>
+        <img
+          ref={ref}
+          src="https://fonster.qodeinteractive.com/wp-content/uploads/2021/04/h1-slider-img-3.jpg"
+          alt="Cool"
+          className="w-[400px] h-[300px] object-fit object-cover"
+        />
       </div>
     </div>
   );
